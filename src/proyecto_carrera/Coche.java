3eb6f28a41ea4f00062333345001282dbc06949e
Oscar <oscarmagallon2002@hotmail.com>
2021-01-29 13:10:11 +0100

    .

--- src/proyecto_carrera/Coche.java
+++ src/proyecto_carrera/Coche.java
@@ -6,5 +6,8 @@ private String nombrePiloto;
 private int dorsal;
 private int distanciaCarrera;
 private String estadoCoche;
+private int potencia;
+private int velocidad;
+private int kilometrosRecorridos;
 
 }
