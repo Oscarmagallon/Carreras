7638fe5783d081943d5f86d9ec77117197a91c7c
Oscar <oscarmagallon2002@hotmail.com>
2021-01-31 20:32:20 +0100

    terminado

--- src/proyecto_carrera/Coche.java
+++ src/proyecto_carrera/Coche.java
@@ -4,131 +4,164 @@ import java.util.Scanner;
 
 public class Coche {
 
-private String nombrePiloto;
-private int dorsal;
-private int distanciaCarrera;
-private String estadoCoche;
-private int potencia;
-private int velocidad;
-private int kilometrosRecorridos;
-private final int POTENCIA=50;
-public Coche(String nombrePiloto, int dorsal) {
-	this.nombrePiloto = nombrePiloto;
-	this.dorsal = dorsal;
-	this.distanciaCarrera= 7500;
-	this.estadoCoche="Parado";
-	this.velocidad=0;
-	this.kilometrosRecorridos=0;
+	private String nombrePiloto;
+	private int dorsal;
+	private int distanciaCarrera;
+	private String estadoCoche;
+	private int potencia;
+	private int velocidad;
+	private int kilometrosRecorridos;
+	private final int POTENCIA = 50;
 
-	
-}
+	public Coche(String nombrePiloto, int dorsal) {
+		this.nombrePiloto = nombrePiloto;
+		this.dorsal = dorsal;
+		this.distanciaCarrera = 7500;
+		this.estadoCoche = "Parado";
+		this.velocidad = 0;
+		this.kilometrosRecorridos = 0;
 
-public String getNombrePiloto() {
-	return nombrePiloto;
-}
-
-public void setNombrePiloto(String nombrePiloto) {
-	this.nombrePiloto = nombrePiloto;
-}
-
-public int getDorsal() {
-	return dorsal;
-}
-
-public void setDorsal(int dorsal) {
-	this.dorsal = dorsal;
-}
-
-public int getDistanciaCarrera() {
-	return distanciaCarrera;
-}
-
-public void setDistanciaCarrera(int distanciaCarrera) {
-	this.distanciaCarrera = distanciaCarrera;
-}
-
-public String getEstadoCoche() {
-	return estadoCoche;
-}
-
-public void setEstadoCoche(String estadoCoche) {
-	this.estadoCoche = estadoCoche;
-}
-
-public int getPotencia() {
-	return potencia;
-}
-
-public void setPotencia(int potencia) {
-	this.potencia = potencia;
-}
-
-public int getVelocidad() {
-	return velocidad;
-}
-
-public void setVelocidad(int velocidad) {
-	this.velocidad = velocidad;
-}
-
-public int getKilometrosRecorridos() {
-	return kilometrosRecorridos;
-}
-
-public void setKilometrosRecorridos(int kilometrosRecorridos) {
-	this.kilometrosRecorridos = kilometrosRecorridos;
-}
-
-public int getPOTENCIA() {
-	return POTENCIA;
-}
-
-public void pintarMenu() {
-	int opc=0;
-	Scanner leer=new Scanner(System.in);
-	do {
-		System.out.println("Menú de opciones para la carrera");
-		System.out.println("Pulsa 1 para arrancar");
-		System.out.println("Pulsa 2 para acelerar");
-		System.out.println("Pulsa 3 para frenar");
-		System.out.println("Pulsa 4 para rearrancar");
-		System.out.println("Pulsa 5 para abanconar la carrera");
-		System.out.println("Escribe un numero:");
-		opc=leer.nextInt();
-		switch (opc) {
-		case 1: estadoCoche="MARCHA";
-		case 4:
-		}
-		
-	} while (opc>0&&opc<6);
-}
-public void arrancar() {
-	
-	
-	switch (this.estadoCoche) {
-	case "PARADO":
-		this.setEstadoCoche("MARCHA");
-		System.out.println("Has arrancado el coche");
-		break;
-	case "MARCHA":
-		System.out.println("Tu coche ya esta en marcha, has perdido el turno");
-		break;
-		
-	case "ACCIDENTADO":
-		System.out.println("Tu coche esta accidentado, debes rearrancarlo en el turno siguiente");
-		break;
-		
-	case "TERMINADO":
-		System.out.println("Ya has terminado la carrera");
-		break;
-	
 	}
-	
+
+	public String getNombrePiloto() {
+		return nombrePiloto;
+	}
+
+	public void setNombrePiloto(String nombrePiloto) {
+		this.nombrePiloto = nombrePiloto;
+	}
+
+	public int getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+
+	public int getDistanciaCarrera() {
+		return distanciaCarrera;
+	}
+
+	public void setDistanciaCarrera(int distanciaCarrera) {
+		this.distanciaCarrera = distanciaCarrera;
+	}
+
+	public String getEstadoCoche() {
+		return estadoCoche;
+	}
+
+	public void setEstadoCoche(String estadoCoche) {
+		this.estadoCoche = estadoCoche;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+	public int getKilometrosRecorridos() {
+		return kilometrosRecorridos;
+	}
+
+	public void setKilometrosRecorridos(int kilometrosRecorridos) {
+		this.kilometrosRecorridos = kilometrosRecorridos;
+	}
+
+	public int getPOTENCIA() {
+		return POTENCIA;
+	}
+
+
+
+	public void arrancar() {
+		if (this.getEstadoCoche().equalsIgnoreCase("PARADO")) {
+			this.setEstadoCoche("MARCHA");
+			System.out.println("Coche arrancado");
+
+		} else if (this.getEstadoCoche().equalsIgnoreCase("MARCHA")) {
+
+			System.out.println("Tu coche ya esta arrancado");
+		} else if (this.getEstadoCoche().equals("ACCIDENTADO")) {
+			this.setEstadoCoche("MARCHA");
+			System.out.println("Coche arrancado");
+		}
+		return;
+	}
+
+public void acelerar() {
+		
+		if ((this.getEstadoCoche().equalsIgnoreCase("Parado"))||(this.getEstadoCoche().equals("Accidentado"))) {
+			System.out.println("Para acelerar primero tienes que arrancar el coche.");
+		}else {
+			if (this.getEstadoCoche().equalsIgnoreCase("Marcha")) {
+				double r = Math.random()*this.POTENCIA+1;
+				int aceleracion = (int)Math.floor(r);
+				int velocidad = this.getVelocidad();
+				int kilometros = this.getKilometrosRecorridos();
+				
+				velocidad += aceleracion;
+				
+				if (velocidad > 200) {
+					this.setVelocidad(0);
+					this.setEstadoCoche("ACCIDENTADO");
+					System.out.println("Te has pasado de velocidad y te la has pegado.");
+				}else {
+					this.setVelocidad(velocidad);
+					this.setKilometrosRecorridos(kilometros+velocidad);
+					System.out.println("Tu velocidad es de " + this.getVelocidad() + " km/h");
+				}
+				if (this.kilometrosRecorridos >= this.distanciaCarrera) {
+					System.out.println("Has terminado la carrera.");
+				}else {
+					System.out.println("Te faltan " + (this.getDistanciaCarrera()- this.getKilometrosRecorridos()) + " kms para terminar la carrera.");
+					return;
+				}
+			}
+		}
+	}
+
+	public void frenar() {
+
+		if ((this.getEstadoCoche().equalsIgnoreCase("Parado")) || (this.getEstadoCoche().equals("Accidentado"))) {
+			System.out.println("Para frenar primero tienes que arrancar el coche.");
+		} else {
+			if (this.getEstadoCoche().equalsIgnoreCase("Marcha")) {
+				double r = Math.random() * this.POTENCIA + 1;
+				int frenada = (int) Math.floor(r);
+				int velocidad = this.getVelocidad();
+				int kms = this.getKilometrosRecorridos();
+
+				velocidad -= frenada;
+
+				if (velocidad < 0) {
+					this.setVelocidad(0);
+					this.setEstadoCoche("Estas parado.");
+					System.out.println("Tu velocidad es de " + this.getVelocidad());
+				} else {
+					this.setVelocidad(velocidad);
+					this.setKilometrosRecorridos(kms + velocidad);
+					System.out.println("Vas a " + this.getVelocidad() + " km/h");
+				}
+				if (this.kilometrosRecorridos >= this.distanciaCarrera) {
+					System.out.println("Has terminado la carrera.");
+				} else {
+					System.out.println("Faltan " + (this.getDistanciaCarrera() - this.kilometrosRecorridos)
+							+ " kms para terminar la carrera.");
+					return;
+				}
+			}
+		}
+	}
 }
-}
-
-
-
-
-
-
--- src/proyecto_carrera/Main.java
+++ src/proyecto_carrera/Main.java
@@ -1,10 +1,60 @@
 package proyecto_carrera;
 
+import java.util.Scanner;
+/*
+ * @author Oscar Magallon
+ */
+
 public class Main {
 
+	public static int pintaMenu() {
+
+		int opc = 0;
+
+		Scanner leer = new Scanner(System.in);
+
+		do {
+			System.out.println("Menu carrera");
+			System.out.println("Pulsa 1 para arrancar el coche.");
+			System.out.println("Pulsa 2 para acelerar");
+			System.out.println("Pulsa 3 para frenar");
+			System.out.println("Pulsa 4 para rearrancar");
+			opc = leer.nextInt();
+			
+		} while (opc < 1 || opc > 4);
+
+		return opc;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
+		int opc = 0;
+
+		Coche c = new Coche("Pepe", 23);
+
+		do {
+			opc = pintaMenu();
+
+			switch (opc) {
+			case 1: {
+				c.arrancar();
+				break;
+			}
+			case 2: {
+				c.acelerar();
+				break;
+			}
+			case 3: {
+				c.frenar();
+				break;
+			}
+			case 4: {
+				c.arrancar();
+				break;
+			}
+			}
+		} while (c.getKilometrosRecorridos() <= c.getDistanciaCarrera());
+
 	}
 
 }
