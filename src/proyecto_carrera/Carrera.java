54d8f020379c73ceec62fbbff850ad051c17ba3d
Oscar <oscarmagallon2002@hotmail.com>
2021-02-02 19:59:54 +0100

    .

--- src/proyecto_carrera/Carrera.java
+++ src/proyecto_carrera/Carrera.java
@@ -3,20 +3,30 @@ package proyecto_carrera;
 import java.util.Scanner;
 
 public class Carrera {
-	private Coche Coche[] = new Coche [10];
+	private Coche vCoche[] = new Coche [10];
 	private String nombre;
 	private int dorsal;
 	
 	
+	
+	public Carrera(proyecto_carrera.Coche[] coche, String nombre, int dorsal) {
+		super();
+		this.vCoche = coche;
+		this.nombre = nombre;
+		this.dorsal = dorsal;
+	}
+
+
+
 	public void anadirCoche() {
 		Scanner leer = new Scanner(System.in);
-		for(int i= 0; i<Coche.length;i++) {
-			if (Coche[i]==null) {
+		for(int i= 0; i<vCoche.length;i++) {
+			if (vCoche[i]==null) {
 				System.out.println("Que nombre tiene el piloto");
 				nombre=leer.next();
 				System.out.println("Que dorsal tiene");
 				dorsal=leer.nextInt();
-				Coche[i]=new Coche(nombre, dorsal,100 );
+				vCoche[i]=new Coche(nombre, dorsal,100 );
 				
 			}
 		}
